708318ea6a39ddac9d3ed44b2fe241e7b00e21c9
naumanm <bodhiandmike@gmail.com>
2015-06-13 12:46:30 -0700

    added player arrays

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function setUp() {
   app.get('/', function(req, res){
     res.render('index.ejs');
   });
+  configSocketIO();
 }
 
 function configSocketIO() {
-  // communication
   io.on('connection', function(socket){
     console.log('a user connected');
 
@@ -46,14 +46,55 @@ function createGameBoard() {
   for (var i = 1; i < 11; i++){
     for (var j = 1; j < 11; j++) {
       gameBoardArr.push(new Cell(false, false, false, j, i));
-
     }
   }
-
   return gameBoardArr;
 }
 
-// main
-setUp();
-configSocketIO();
-gameBoard = createGameBoard();
+function createTeam() {
+  var teamArr = [];
+
+  function pieceObj(label, value, alive, x, y) {
+    this.label = label;
+    this.value = value;
+    this.alive = alive;
+    this.x = x;
+    this.y = y;
+  }
+
+  // make the one off pieces
+  teamArr.push(new pieceObj('Flag', -1, false, null, null));
+  teamArr.push(new pieceObj('Marshal', 1, false, null, null));
+  teamArr.push(new pieceObj('Spy', 0, false, null, null));
+  teamArr.push(new pieceObj('General', 2, false, null, null));
+
+  // make the rest
+  addPieceToArray('Colonel', 2, 3);
+  addPieceToArray('Major', 3, 4);
+  addPieceToArray('Captain', 4, 5);
+  addPieceToArray('Lieutenant', 4, 6);
+  addPieceToArray('Sergeant', 4, 7);
+  addPieceToArray('Miner', 5, 8);
+  addPieceToArray('Scout', 8, 9);
+  addPieceToArray('BOMB', 6, 0);
+
+  function addPieceToArray(label, teamCount, value) {
+    for(var i = 1; i < teamCount+1; i++) {
+      var newLabel = label + i;
+      teamArr.push(new pieceObj(newLabel, value, false, null, null));
+    }
+  }
+  return teamArr;
+}
+
+function startGame() {
+  setUp();
+  gameBoard = createGameBoard();
+  teamA = createTeam();
+  teamB = createTeam();
+}
+
+
+// MAIN
+
+startGame();
