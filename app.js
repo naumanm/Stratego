be23185947190935f8d67b9b47d4b0356eed387e
naumanm <bodhiandmike@gmail.com>
2015-06-13 11:37:21 -0700

    gameboard object created

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
-// Stratego, mikeNauman
+// Stratego, by mikeNauman
 
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set("view engine", "ejs");
+function setUp() {
+  app.set("view engine", "ejs");
+  app.use(express.static(__dirname + '/public'));
+  app.get('/', function(req, res){
+    res.render('index.ejs');
+  });
+}
 
-// location for static files
-app.use(express.static(__dirname + '/public'));
+function configSocketIO() {
+  // communication
+  io.on('connection', function(socket){
+    console.log('a user connected');
 
-console.log(__dirname + '/public');
+    socket.on('connectToGame', function(msg){
+      console.log(msg);
+    });
 
-app.get('/', function(req, res){
-  res.render('index.ejs');
-});
-
-// communication
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('connectToGame', function(msg){
-    console.log(msg);
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
   });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  http.listen(3000, function(){
+    console.log('listening on *:3000');
   });
-});
+}
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+function createGameBoard() {
+  var gameBoardArr = [];
+
+  function Cell(occupied, team, value, x, y) {
+    this.x = x;
+    this.y = y;
+    this.occupied = occupied;
+    this.team = team;
+    this.value = value;
+  }
+
+  for (var i = 1; i < 11; i++){
+    for (var j = 1; j < 11; j++) {
+      gameBoardArr.push(new Cell(false, false, false, j, i));
+
+    }
+  }
+
+  return gameBoardArr;
+}
+
+// main
+setUp();
+configSocketIO();
+gameBoard = createGameBoard();
