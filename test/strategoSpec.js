b01710b7273ed0617bc7e10a5efa317f92e127cd
naumanm <bodhiandmike@gmail.com>
2015-07-04 07:32:58 -0700

    added second test to check Board is locked

--- test/strategoSpec.js
+++ test/strategoSpec.js
@@ -1,12 +1,33 @@
 // stratego smoke test by: heyMikeNauman
 
 describe('stratego smoke test', function() {
-  it('should enter player name', function() {
+
+  beforeEach(function() {
     browser.driver.get('http://localhost:3000/');
-    browser.driver.findElement(by.id('textArea')).sendKeys('Mike');
-    browser.driver.findElement(by.id('nameButton')).click();
+  });
+
+  xit('should enter player name', function() {
+    enterName()
 
     var message = browser.driver.findElement(by.id('gameMessage')).getText();
     expect(message).toEqual('Place your pieces')
   });
+
+  it('should click the place pieces button', function() {
+    enterName();
+    clickPlacePieces();
+
+    var message = browser.driver.findElement(by.id('gameMessage')).getText();
+    expect(message).toEqual('Board is locked')
+  });
+
+  function enterName() {
+    browser.driver.findElement(by.id('textArea')).sendKeys('Mike');
+    browser.driver.findElement(by.id('nameButton')).click();
+  }
+
+  function clickPlacePieces() {
+    browser.driver.findElement(by.id('readyButton')).click();
+  }
+
 });
